e9879bdf8fd3da2d6c56b950d94c42c200cf406b
AzizulHakim18 <hakimrony18@gamil.com>
2022-11-30 14:46:24 +0600

    about updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>Secret about us!!!</h2>
+            <h2>This is About section////</h2>
         </div>
     );
 };
